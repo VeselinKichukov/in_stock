4a1d6bfe8243ab2a1f5f71cc695ab11907fd724e
VeselinKichukov <veskichukov@gmail.com>
2020-05-16 16:18:39 +0200

    Add product tracking.

---
+++ tests/Feature/TrackCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Product;
+use App\Retailer;
+use App\Stock;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use RetailerWithProductSeeder;
+use Tests\TestCase;
+use function foo\func;
+
+class TrackCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_tracks_product_stock()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        $this->assertFalse(Product::first()->inStock());
+
+        Http::fake(fn() => ['available' => true, 'price' => 29900]);
+
+        $this->artisan('track')
+            ->expectsOutput('All done!');
+
+        $this->assertTrue(Product::first()->inStock());
+    }
+}
