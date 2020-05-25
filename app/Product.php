b2067f163c5cd2728ed4df663d913fc8a654de21
VeselinKichukov <veskichukov@gmail.com>
2020-05-25 22:03:55 +0200

    Add product history functionality.

--- app/Product.php
+++ app/Product.php
@@ -6,15 +6,32 @@ class Product extends Model
 {
     public function track()
     {
-        $this->stock->each->track();
+        $this->stock->each->track(
+            fn($stock) => $this->recordHistory($stock)
+        );
     }
+
     public function inStock()
     {
-      return $this->stock()->where('in_stock',true)->exists();
+        return $this->stock()->where('in_stock', true)->exists();
     }
 
     public function stock()
     {
-       return $this->hasMany(Stock::class);
+        return $this->hasMany(Stock::class);
+    }
+
+    public function recordHistory(Stock $stock)
+    {
+        $this->history()->create([
+            'price' => $stock->price,
+            'stock_id' => $stock->id,
+            'in_stock' => $stock->in_stock,
+        ]);
+    }
+
+    public function history()
+    {
+        return $this->hasMany(History::class);
     }
 }
---
+++ tests/Unit/ProductHistoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Clients\StockStatus;
+use App\Product;
+use Facades\App\Clients\ClientFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use RetailerWithProductSeeder;
+use Tests\TestCase;
+
+class ProductHistoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_records_history_each_time_a_stock_is_tracked()
+    {
+        $this->seed(RetailerWithProductSeeder::class);
+
+        ClientFactory::shouldReceive('make->checkAvailability')
+            ->andReturn(new StockStatus($available = true, $price = 99));
+
+        $product = tap(Product::first(), function($product){
+            $this->assertCount(0, $product->history);
+
+            $product->track();
+
+            $this->assertCount(1, $product->refresh()->history);
+        });
+
+        $history = $product->history->first();
+        $this->assertEquals($price,$history->price);
+        $this->assertEquals($available,$history->in_stock);
+        $this->assertEquals($product->id,$history->product_id);
+        $this->assertEquals($product->stock[0]->id,$history->stock_id);
+    }
+}
